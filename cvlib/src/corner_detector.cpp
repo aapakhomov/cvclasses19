17d565a05e5fde13a984e2a55a1a89e7ff65287b
Aleksei Pakhomov <lkspakhomov@gmail.com>
2024-11-25 01:49:03 +0300

    add lab 4 solutuon

--- cvlib/src/corner_detector.cpp
+++ cvlib/src/corner_detector.cpp
@@ -16,15 +16,64 @@ cv::Ptr<corner_detector_fast> corner_detector_fast::create()
     return cv::makePtr<corner_detector_fast>();
 }
 
+bool test_candidate(const cv::Mat& img, cv::Point point, const std::vector<cv::Point>& offsets, int pixel_num_threshold)
+{
+    int count = 0;
+    const int threshold = 30;
+    const auto pixel = img.at<uint8_t>(point);
+    for (const auto& offset : offsets)
+    {
+        const auto offset_pixel = img.at<uint8_t>(point + offset);
+        if (offset_pixel < pixel - threshold || offset_pixel > pixel + threshold) ++count;
+        if (count >= pixel_num_threshold) return true;
+    }
+    return false;
+}
+
 void corner_detector_fast::detect(cv::InputArray image, CV_OUT std::vector<cv::KeyPoint>& keypoints, cv::InputArray /*mask = cv::noArray()*/)
 {
     keypoints.clear();
-    // \todo implement FAST with minimal LOCs(lines of code), but keep code readable.
+    const auto img_ = image.getMat();
+    cv::Mat img;
+    img_.copyTo(img);
+    if (img.channels() == 3)
+        cv::cvtColor(img, img, cv::COLOR_BGR2GRAY);
+
+    // точки для проверки, представленные как смещения от тестируемой точки
+    // первая группа пикселей (крест) проверяется в начале, если предикат не выполнен, процедура завершается
+    // если выполнен, проверяются оставшиеся пиксели
+    const auto offsets_init = std::vector<cv::Point>{
+        cv::Point(0, 3), 
+        cv::Point(3, 0), 
+        cv::Point( 0, -3), 
+        cv::Point(-3, 0)
+        };
+    const auto offsets_residual = std::vector<cv::Point>{
+        cv::Point(-1, 3), cv::Point(1, 3),
+        cv::Point(2, 2), 
+        cv::Point(3, 1), cv::Point(3, -1), 
+        cv::Point(2, -2),
+        cv::Point(1, -3), cv::Point(-1, -3),
+        cv::Point(-2, -2),
+        cv::Point(-3, -1), cv::Point(-3, 1),
+        cv::Point(-2, 2)
+        };
+
+    // крайние 3 ряда пикселей не обрабатываются, т.к. окно не помещается на изображении
+    for (auto row = 3; row < img.rows - 3; ++row)
+    {
+        for (auto col = 3; col < img.cols - 3; ++col)
+        {
+            const auto center = cv::Point(col, row);
+            if (test_candidate(img, center, offsets_init, 3) && test_candidate(img, center, offsets_residual, 12)) {
+                keypoints.push_back(cv::KeyPoint(center, 3));
+            }
+        }
+    }
 }
 
 void corner_detector_fast::compute(cv::InputArray, std::vector<cv::KeyPoint>& keypoints, cv::OutputArray descriptors)
 {
-    std::srand(unsigned(std::time(0))); // \todo remove me
     // \todo implement any binary descriptor
     const int desc_length = 2;
     descriptors.create(static_cast<int>(keypoints.size()), desc_length, CV_32S);
@@ -42,8 +91,10 @@ void corner_detector_fast::compute(cv::InputArray, std::vector<cv::KeyPoint>& ke
     }
 }
 
-void corner_detector_fast::detectAndCompute(cv::InputArray, cv::InputArray, std::vector<cv::KeyPoint>&, cv::OutputArray descriptors, bool /*= false*/)
+void corner_detector_fast::detectAndCompute(cv::InputArray image, cv::InputArray, std::vector<cv::KeyPoint>& keypoints, cv::OutputArray descriptors, bool /*= false*/)
 {
     // \todo implement me
+    this->detect(image, keypoints);
+    this->compute(image, keypoints, descriptors);
 }
 } // namespace cvlib
--- demo/demo_corner_detector.cpp
+++ demo/demo_corner_detector.cpp
@@ -22,7 +22,7 @@ int demo_corner_detector(int argc, char* argv[])
     cv::namedWindow(demo_wnd);
 
     cv::Mat frame;
-    auto detector = cv::GFTTDetector::create(); // \todo use cvlib::corner_detector_fast
+    auto detector = cvlib::corner_detector_fast::create();
     std::vector<cv::KeyPoint> corners;
 
     utils::fps_counter fps;
@@ -35,6 +35,12 @@ int demo_corner_detector(int argc, char* argv[])
         cv::drawKeypoints(frame, corners, frame, cv::Scalar(0, 0, 255));
         utils::put_fps_text(frame, fps);
         // \todo add count of the detected corners at the top left corner of the image. Use green text color.
+        cv::putText(frame, 
+            "number of corners: " + std::to_string(corners.size()), 
+            cv::Point(25, 25), // top-left position
+            cv::FONT_HERSHEY_SIMPLEX, 
+            1.0, 
+            CV_RGB(0, 255, 0));
         cv::imshow(demo_wnd, frame);
     }
 
