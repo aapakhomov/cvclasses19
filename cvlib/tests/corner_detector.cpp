a80c9eee359d8c628889040cb90b4077ba8e75e7
Aleksei Pakhomov <lkspakhomov@gmail.com>
2024-11-25 01:24:17 +0300

    add lab 4 solution0

--- cvlib/tests/corner_detector.cpp
+++ cvlib/tests/corner_detector.cpp
@@ -13,7 +13,7 @@ using namespace cvlib;
 TEST_CASE("simple check", "[corner_detector_fast]")
 {
     auto fast = corner_detector_fast::create();
-    cv::Mat image(10, 10, CV_8UC1);
+    cv::Mat image(10, 10, CV_8UC1, cv::Scalar(0));
     SECTION("empty image")
     {
         std::vector<cv::KeyPoint> out;
@@ -22,4 +22,82 @@ TEST_CASE("simple check", "[corner_detector_fast]")
     }
 
     // \todo add 5 or more tests (SECTIONs)
+    // 
+    SECTION("constant")
+    {
+        const auto image = cv::Mat(3, 3, CV_8UC1, cv::Scalar(127));
+        std::vector<cv::KeyPoint> out;
+        fast->detect(image, out);
+        REQUIRE(out.empty());
+    }
+
+    SECTION("^")
+    {
+        cv::Mat image = (cv::Mat_<uint8_t>(7, 7) <<
+                15, 15, 15, 15, 15, 15, 15,
+                15, 15, 15, 15, 15, 15, 15,
+                15, 15, 15, 15, 15, 15, 15,
+                15, 15, 15, 228, 15, 15, 15,
+                15, 15, 228, 228, 228, 15, 15,
+                15, 15, 228, 15, 228, 15, 15,
+                15, 228, 15, 15, 15, 228, 15
+                );
+        std::vector<cv::KeyPoint> out;
+        fast->detect(image, out);
+        REQUIRE(1 == out.size());
+        REQUIRE(out[0].pt.x == 3);
+        REQUIRE(out[0].pt.y == 3);
+    }
+
+    SECTION("v")
+    {
+        cv::Mat image = (cv::Mat_<uint8_t>(7, 7) <<
+                15, 228, 15, 15, 15, 15, 15,
+                15, 15, 228, 15, 15, 15, 228,
+                15, 15, 228, 15, 228, 228, 15,
+                15, 15, 15, 228, 15, 15, 15,
+                15, 15, 15, 15, 15, 15, 15,
+                15, 15, 15, 15, 15, 15, 15,
+                15, 15, 15, 15, 15, 15, 15
+                );
+        std::vector<cv::KeyPoint> out;
+        fast->detect(image, out);
+        REQUIRE(1 == out.size());
+        REQUIRE(out[0].pt.x == 3);
+        REQUIRE(out[0].pt.y == 3);
+    }
+
+    SECTION("<")
+    {
+        cv::Mat image = (cv::Mat_<uint8_t>(7, 7) <<
+                15, 15, 15, 15, 15, 15, 15,
+                15, 15, 15, 15, 15, 15, 228,
+                15, 15, 15, 15, 228, 228, 15,
+                15, 15, 15, 228, 15, 15, 15,
+                15, 15, 15, 15, 228, 228, 15,
+                15, 15, 15, 15, 15, 15, 228,
+                15, 15, 15, 15, 15, 15, 15
+                );
+        std::vector<cv::KeyPoint> out;
+        fast->detect(image, out);
+        REQUIRE(1 == out.size());
+        REQUIRE(out[0].pt.x == 3);
+        REQUIRE(out[0].pt.y == 3);
+    }
+
+    SECTION("strip")
+    {
+        cv::Mat image = (cv::Mat_<uint8_t>(7, 7) <<
+                15, 15, 228, 228, 228, 15, 15,
+                15, 15, 228, 228, 228, 15, 15,
+                15, 15, 228, 228, 228, 15, 15,
+                15, 15, 228, 228, 228, 15, 15,
+                15, 15, 228, 228, 228, 15, 15,
+                15, 15, 228, 228, 228, 15, 15,
+                15, 15, 228, 228, 228, 15, 15
+                );
+        std::vector<cv::KeyPoint> out;
+        fast->detect(image, out);
+        REQUIRE(out.empty());
+    }
 }
